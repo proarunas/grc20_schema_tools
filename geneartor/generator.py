b46d0d023173e8c750e61f6f5e1802873b073550
Arūnas Prokopas <arunas+24fxx6j3@proko.dev>
2024-12-19 09:48:53 +0100

    Cleaned up the file structure.

--- geneartor/generator.py
+++ geneartor/generator.py
@@ -17,10 +17,10 @@ main_schema_path = os.path.join(schema_dir, "base_schema.yaml")
 value_types_path = os.path.join(schema_dir, "value_types.yaml")
 
 output_dir = "../generated/"
-summary_file_path = os.path.join(output_dir, "simple_type_summary.yaml")
-attribute_summary_path = os.path.join(output_dir, "simple_attribute_summary.yaml")
+summary_file_path = os.path.join(output_dir, "type_summary.yaml")
+attribute_summary_path = os.path.join(output_dir, "attribute_summary.yaml")
 rel_summary_path = os.path.join(output_dir, "relation_summary.yaml")
-id_dict_file_path = os.path.join(output_dir, "simple_id_dict.yaml")
+id_dict_file_path = os.path.join(output_dir, "id_dict.yaml")
 
 debug = False
 
